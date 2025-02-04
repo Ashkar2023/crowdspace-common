aea5de12614a06bc2507932531ff82aa3359d918
ashkar2023 <muhammedashkar2023@gmail.com>
2025-02-04 15:04:46 +0530

    chore: changed name of env validator function

--- src/core/env.validator.ts
+++ src/core/env.validator.ts
@@ -1,9 +1,11 @@
 import { EnvNotFoundError } from "./extended.errors.js";
 
-export const envValidator = (envObj: { [key: string]: string }, serviceName:string) => {
+export const validateEnv = (envObj: { [key: string]: string }, serviceName:string) => {
     for (let key in envObj) {
         if (envObj[key] === undefined) {
             throw new EnvNotFoundError(key, serviceName);
         }
     }
+
+    console.log("ENV's set correctly");
 }
