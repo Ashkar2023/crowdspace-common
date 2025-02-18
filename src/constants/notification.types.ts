0b0e29842ace2ec67fc6a90759babb224fd0b504
ashkar2023 <muhammedashkar2023@gmail.com>
2025-02-18 13:05:26 +0530

    feat: added followReqAccept NotificationKind

--- src/constants/notification.types.ts
+++ src/constants/notification.types.ts
@@ -1,5 +1,6 @@
 export enum NotificationKind {
     followRequest = "follow_request",
+    followRequestAccepted = "follow_request_accepted",
     follow = "follow",
     like = "like",
     comment = "comment",
