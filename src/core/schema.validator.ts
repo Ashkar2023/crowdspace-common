639680afed5ad48868ea3e20f489f36464cc1780
ashkar2023 <muhammedashkar2023@gmail.com>
2025-02-20 17:53:04 +0530

    feat(SchemaValidator): coerceAndValidate method

--- src/core/schema.validator.ts
+++ src/core/schema.validator.ts
@@ -16,4 +16,18 @@ export class SchemaValidator implements ISchemaValidator {
             }
         }
     }
+    
+    coerceAndValidate<I,T>(input:I, schema: ZodType<T>): boolean | never {
+        try {
+            const result = schema.parse(input);
+            return true;
+        } catch (error) {
+            if (error instanceof ZodError) {
+                const { _errors: [message] } = error.format();
+                throw new ValidationError(message);
+            } else {
+                throw new ValidationError('An unknown error occurred at common/validation');
+            }
+        }
+    }
 }
