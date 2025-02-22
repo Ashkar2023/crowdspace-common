24e52d3d6f5368189d4f8848d63dc24596e8284a
ashkar2023 <muhammedashkar2023@gmail.com>
2025-02-22 10:33:18 +0530

    refactor: update digest to base64url, remove regex basedstring manipulation

--- src/core/crypto.util.ts
+++ src/core/crypto.util.ts
@@ -4,9 +4,6 @@ export const generateUrlSafeHash = (...args: [string, string]) => {
 
     return createHash('sha256')
         .update(args.join(""))
-        .digest("base64")
-        .replace(/[+/]/g, (match) => {
-            return match === "+" ? "-" : "_";
-        })
-        .replace(/=+$/, "");
+        .digest("base64url")
+
 }
