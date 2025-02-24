e469f78acc0e1be76d96c5dceb4ab77a7e70ee32
ashkar2023 <muhammedashkar2023@gmail.com>
2025-02-24 13:12:29 +0530

    feat: generate password crypto util
    crypto util fns merged, removed generateOtp file

--- src/core/crypto.util.ts
+++ src/core/crypto.util.ts
@@ -1,4 +1,6 @@
-import { createHash } from "node:crypto";
+import { createHash, randomBytes } from "node:crypto";
+import { TGenerateOtp } from "@interfaces/utils.interface.js";
+
 
 export const generateUrlSafeHash = (...args: [string, string]) => {
 
@@ -7,3 +9,45 @@ export const generateUrlSafeHash = (...args: [string, string]) => {
         .digest("base64url")
 
 }
+
+/**
+ * Generates a 4-digit OTP (One-Time Password) asynchronously.
+ *
+ * This function uses the `randomBytes` method from the Node.js `crypto` module to generate
+ * a 3-byte random value, which is then converted to a hexadecimal string and further processed
+ * to create a 4-digit OTP.
+ *
+ * @throws {Error} If there is an error during the generation of random bytes.
+ *
+ * @example
+ * generateOTP().then(otp => {
+ *   console.log(otp); // Logs the generated OTP
+ * }).catch(err => {
+ *   console.error(err); // Handle any errors
+ * });
+ */
+export const generateOTP :TGenerateOtp = async () => new Promise((resolve, reject) => {
+
+    randomBytes(3, (err, buffer) => {
+        if (err) {
+            reject(err);
+        } else {
+            const otp = parseInt(buffer.toString("hex"), 16)
+                .toString().substring(0, 4)
+
+            resolve(otp)
+        }
+    })
+})
+
+export const generatePassword = async () => new Promise((resolve, reject) => {
+
+    randomBytes(12, (err, buffer) => {
+        if (err) {
+            reject(err);
+        } else {
+            const password = buffer.toString("base64url").substring(0, 18);
+            resolve(password);
+        }
+    })
+})
